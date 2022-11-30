e3f24b554cace874cb4cab23c4a8f8a60bbf8b72
ahnaf4749 <ahnafsagor12@gmail.com>
2022-11-30 23:44:31 +0600

    navbar change

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
         </React.Fragment>
 
     return (
-        <div className="navbar bg-base-100 flex justify-between">
+        <div className="navbar bg-base-300 rounded-sm flex justify-between">
             <div className="navbar-start ">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
